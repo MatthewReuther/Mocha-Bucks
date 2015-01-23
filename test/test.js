ea1681392c2ca419e5c448076a6cc54b61c25268
Matthew Reuther <matthew.reuther@gmail.com>
2015-01-23 13:31:23 -0600

    first-test

---
+++ test/test.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+/* jshint expr: true */
+
+'use strict';
+
+var expect = require('chai').expect;
+
+describe ('Array', function () {
+  it ('should expect', function () {
+    expect(true).to.be.true;
+    expect(false).to.be.false;
+
+  });
+});
